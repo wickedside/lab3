0bab73d0dfc80da4d55f28b2722ed491693978c4
wickedside <w1ck3ds1d3@gmail.com>
2023-03-19 20:41:05 +0300

    editted ScrollingScreen.kt

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
@@ -1,2 +1,38 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.lab1.router.BackButtonHandler
+import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
+
+@Composable
+fun ScrollingScreen () {
+    MyScrollingScreen()
+
+    BackButtonHandler {
+        FundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MyScrollingScreen(modifier: Modifier = Modifier) {}
+
+@Composable
+fun BookImage(@DrawableRes imageResId: Int, @StringRes contentDescriptionResId: Int){
+    Image(
+        bitmap = ImageBitmap.imageResource(imageResId),
+        contentDescription = stringResource(contentDescriptionResId),
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier.size(476.dp, 616.dp)
+    )
+}
