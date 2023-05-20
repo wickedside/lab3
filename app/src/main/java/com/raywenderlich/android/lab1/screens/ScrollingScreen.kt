f5674f8fcd840b9f79817ed5bf47885199161adf
wickedside <w1ck3ds1d3@gmail.com>
2023-05-20 12:32:34 +0300

    edited ScrollingScreen.kt, added horizontalScroll

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
@@ -3,7 +3,9 @@ package com.raywenderlich.android.lab1.screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -30,7 +32,7 @@ fun ScrollingScreen () {
 
 @Composable
 fun MyScrollingScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
         BookImage(R.drawable.advanced_architecture_android, R.string.architecture)
         BookImage(R.drawable.kotlin_aprentice, R.string.learn_kotlin)
         BookImage(R.drawable.kotlin_coroutines, R.string.coroutines)
